9bbba294497aabfc365ab0892a39955dc32bc7ef
MehdiHenna <mehdihennaoui@gmail.com>
2016-05-30 15:59:30 +0200

    Faire fonctionner les routes pour home et cards

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -8,7 +8,7 @@ use App\Models\Product;
 class HomeController extends Controller {
 
 	public function getIndex(Request $request, Application $app){
-		//return view('home', ['products' => Product::all()]);
+		return view('home', ['products' => Product::all()]);
 	}	
 
 	public function getLogout(Application $app){

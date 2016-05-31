f67904de6aef4ef5e8902b286288091d96c16939
MehdiHenna <mehdihennaoui@gmail.com>
2016-05-31 10:01:14 +0200

    ajout de la vue des produits sur la page home

--- views/home.php
+++ views/home.php
@@ -5,5 +5,20 @@
 	<hr class="ui divider">
 	<section class="ui three stackable cards">
 		<!-- boucle des produits ui card -->
+		<?php foreach ($products as $product): ?>
+			<div class="card">
+				<div class="image">
+					<img src="<?= $product->picture ?>">				
+				</div>
+				<div class="header">
+					<h3><?= $product->name ?></h3>
+				</div>
+				<p class="description"><?= $product->price ?>€</p>
+			<div class="ui bottom attached animated button" tabindex="0">Add</div>
+			</div>
+				
+			
+		<?php endforeach;?>
 	</section>
 </div>
+
